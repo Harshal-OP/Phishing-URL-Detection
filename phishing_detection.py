e39bb9aa9ad3452e4f855ddb2ca91902e623cd91
Harshal <75478201+Harshal-OP@users.noreply.github.com>
2023-03-29 00:46:45 +0530

    Add files via upload

---
+++ phishing_detection.py
@@ -0,0 +1,119 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import precision_score, recall_score, confusion_matrix, classification_report, accuracy_score, f1_score, ConfusionMatrixDisplay
+import warnings
+warnings.filterwarnings("ignore")
+import random
+random.seed(42)
+
+df = pd.read_csv('/content/url_updated.csv')
+
+print('Here is the baseline : \n',df['phishing'].value_counts(normalize=True))
+
+X = df.drop(columns=['url', 'protocol', 'domain', 'path', 'query', 'fragment','phishing'])
+
+y = df['phishing']
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,random_state = 42, stratify = y )
+
+ss = StandardScaler()
+
+X_train_sc = ss.fit_transform(X_train)
+
+X_test_sc = ss.transform(X_test)
+
+def get_scores_sc(model):
+    
+    # instantiate model
+    model = model()
+    
+    # fit to scaled data
+    model.fit(X_train_sc, y_train)
+    
+    # make predictions
+    preds = model.predict(X_test_sc)
+
+    # print results
+    print(f'{model} Training Score: {model.score(X_train_sc, y_train)}')
+    print(f'{model} Testing Score: {model.score(X_test_sc, y_test)}')
+    print()
+    print(classification_report(y_test, preds))
+    print(confusion_matrix(y_test, preds))
+
+    return
+
+def get_scores(model):
+    
+    # instantiate model
+    model = model()
+    
+    # fit to data
+    model.fit(X_train, y_train)
+    
+    # make predictions
+    preds = model.predict(X_test)
+
+    # print results
+    print(f'{model} Training Score: {model.score(X_train, y_train)}')
+    print(f'{model} Testing Score: {model.score(X_test, y_test)}')
+    print()
+    print(classification_report(y_test, preds))
+    print(confusion_matrix(y_test, preds))
+
+    return
+
+get_scores_sc(DecisionTreeClassifier)
+
+get_scores(ExtraTreesClassifier)
+
+get_scores(RandomForestClassifier)
+
+!pip install streamlit
+
+import streamlit as st
+
+@st.cache
+def get_scores(model):
+    
+    # instantiate model
+    model = model()
+    
+    # fit to data
+    model.fit(X_train, y_train)
+    
+    # make predictions
+    preds = model.predict(X_test)
+
+    # print results
+    print(f'{model} Training Score: {model.score(X_train, y_train)}')
+    print(f'{model} Testing Score: {model.score(X_test, y_test)}')
+    print()
+    print(classification_report(y_test, preds))
+    print(confusion_matrix(y_test, preds))
+
+    return
+
+st.title("Phishing Detection Model")
+
+# Create sidebar
+st.sidebar.title("Input Values")
+
+# Create main page
+st.write("Here you can input values and get the output of the model")
+
+# Create input slider
+num_input = st.sidebar.slider("Please select numbers", min_value=0, max_value=100, value=0)
+
+# Create submit button
+submit_button = st.sidebar.button("Submit")
+
+# Create main page
+st.write("Your output is:")
+
+# Create output
+if submit_button:
+    get_scores(RandomForestClassifier)
+    st.write(num_input)
